684d3dad29d059179af598a5c879150393741bc2
MatiasSola <msola@itba.edu.ar>
2023-05-30 20:53:06 -0300

    new

---
+++ javascript.js
@@ -0,0 +1,14 @@
+console.log("Hola")
+
+for(let x=0; x < 10; x++){
+    console.log(x)
+}
+
+let array1 = [0,1,2,3]
+for( let x=0; x<4; x++){
+    console.log(array1[x])
+}
+
+for(let x=0; x<array1.length; x++){
+    console.log(array1[x])
+}
